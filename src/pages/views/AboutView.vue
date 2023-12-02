dc1bc9a0a7347137d27d7902f47d8ee5bc207414
Dávid Soóky <sooky.david98@gmail.com>
2023-12-03 00:56:49 +0100

    fix: about page

--- src/pages/views/AboutView.vue
+++ src/pages/views/AboutView.vue
@@ -1,7 +1,14 @@
 <template>
   <section>
     <base-card>
-      <h1>This is an About page</h1>
+      <h1>Vitajte v OLP </h1>
+      <h2>Vašej bráne k dokonalému online vzdelávaniu!</h2>
+      <br>
+      <br>
+      <h4>Kto sme?</h4>
+      <h5>OLP je online vzdelávacia platforma pre študentov.
+        Vyvinuli ju študenti (Gábor Katona, Patrik Zigo, Dávid Soóky a Orosz Zsombor)
+        na FEI STU ako zadanie pre predmet s názvom I-ASOS.</h5>
     </base-card>
   </section>
 </template>
